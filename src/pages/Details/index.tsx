ffb67bedfc179f1f92afa2d13530617919c369a0
Le-Polemil <paul-emile.moreau@elao.com>
2025-04-10 09:28:40 +0200

    feat: new transition on details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,31 +1,64 @@
-import { useNavigate, useParams } from 'react-router'
+import { Link, useParams } from 'react-router'
 import { CARDS } from '../../constants'
-import Add from '../../components/icons/Add'
 import ArrowDownLeft from '../../components/icons/ArrowDownLeft'
 import './index.scss'
+import ThumbsUp from '../../components/icons/ThumbsUp'
+import Applause from '../../components/icons/Applause'
+import Add from '../../components/icons/Add'
+import { useState } from 'react'
+import { flushSync } from 'react-dom'
+
+const STEPS = [
+  {
+    icon: ThumbsUp,
+    position: "col-3! row-3"
+  },
+  {
+    icon: Add,
+    position: "col-1! row-1"
+  },
+  {
+    icon: Applause,
+    position: "col-3! row-1"
+  },
+]
 
 function Details() {
   const { id } = useParams()
-  const navigate = useNavigate()
   const card = CARDS.find((card) => card.id === id)
+  const [stepId, setStepId] = useState(0)
+  const StepIcon = STEPS[stepId].icon
+  const stepPosition = STEPS[stepId].position
+
+  function goNextStep() {
+    document.startViewTransition(() => {
+      flushSync(() => {
+        setStepId((stepId + 1) % STEPS.length)
+      })
+    })
+  }
 
   return (
     <main id="details">
       <div
         data-id={id}
-        className={`card ${card?.bg} rounded-lg flex flex-col items-center`}
-        style={{ width: '35ch', height: '45ch', viewTransitionName: `card-${id}` }}
+        className={[`card rounded-lg items-center relative w-[35ch] h-[45ch] overflow-clip`, card?.bg].filter(Boolean).join(' ')}
+        style={{ viewTransitionName: `card-${id}` }}
       >
-        <button className="col-1!" onClick={() => document.startViewTransition(() => navigate('/'))}>
+        <div className="ignore-col-padding row-[1/-1] h-full w-full z-0">
+          <img src={card?.image} className="h-full w-full img" alt="Illustration" />
+        </div>
+
+        {/* @ts-expect-error to accept -1 as a goback button */}
+        <Link to={-1} viewTransition className="col-1! row-3! z-10 arrow-icon">
           <ArrowDownLeft className="w-8 h-8" />
-        </button>
-        <button className="col-3!">
-          <Add className="w-8 h-8 add-icon" />
+        </Link>
+
+        <button className={["step-icon z-10", stepPosition].filter(Boolean).join(' ')} onClick={goNextStep}>
+          <StepIcon className={["w-8 h-8", stepId === 0 && 'thumbs-up-icon'].filter(Boolean).join(' ')} />
         </button>
         
-        <img src={card?.image} className="logo w-24 h-24" alt="Vite logo" />
-
-        <h1 className="card-title">{card?.name}</h1>
+        <h1 className="card-title row-2 z-10">{card?.name}</h1>
       </div>
     </main>
   )
