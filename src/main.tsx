15d0c4cbf1505492c2f7d196dcf43d72ab8e010b
Le-Polemil <paul-emile.moreau@elao.com>
2025-04-10 09:09:40 +0200

    fix: use this kind of router to allow the utilisation of view transitions with react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import App from './pages/App'
 import Details from './pages/Details';
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // loader: rootLoader,
+    children: [
+      {
+        index: true,
+        Component: App,
+      },
+      {
+        path: ":id",
+        Component: Details,
+        // loader: teamLoader,
+      },
+    ],
+  },
+]);
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/:id" element={<Details />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 )
