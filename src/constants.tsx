430084e896cacba31d72f42b012e376e0bf27b62
Le-Polemil <paul-emile.moreau@elao.com>
2025-04-10 09:10:05 +0200

    feat: dragons instead of playlists

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,41 +1,45 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import saphira from './assets/saphira.png'
+import thorn from './assets/thorn.png'
+import firnen from './assets/firnen.png'
+import glaedr from './assets/glaedr.png'
+import iormungr from './assets/iormungr.png'
+import shruikan from './assets/shruikan.png'
 
 export const CARDS = [
   {
     id: 'saphira',
     name: 'Saphira',
-    image: viteLogo,
+    image: saphira,
     bg: 'bg-blue-500',
+  },
+  {
+    id: 'glaedr',
+    name: 'Glaedr',
+    image: glaedr,
+    bg: 'bg-yellow-500',
   },
   {
     id: 'thorn',
     name: 'Thorn',
-    image: reactLogo,
+    image: thorn,
     bg: 'bg-red-500',
   },
   {
     id: 'firnen',
     name: 'Fírnen',
-    image: viteLogo,
+    image: firnen,
     bg: 'bg-green-500',
-  },
-  {
-    id: 'glaedr',
-    name: 'Glaedr',
-    image: reactLogo,
-    bg: 'bg-yellow-500',
-  },
-  {
-    id: 'iormungr',
-    name: 'Iormúngr',
-    image: viteLogo,
-    bg: 'bg-slate-300',
   },
   {
     id: 'shruikan',
     name: 'Shruikan',
-    image: reactLogo,
+    image: shruikan,
     bg: 'bg-stone-900',
-  }
+  },
+  {
+    id: 'iormungr',
+    name: 'Iormúngr',
+    image: iormungr,
+    bg: 'bg-amber-600',
+  },
 ] as const
